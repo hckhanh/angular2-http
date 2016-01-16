df97e2af5f689e6185df3a342999c6c85eacf137
Khanh Hoang <Khanh Hoang>
2016-01-17 00:42:20 +0700

    Change to Production Mode

--- component/boot.ts
+++ component/boot.ts
@@ -1,6 +1,6 @@
 import {bootstrap} from 'angular2/platform/browser';
-// import {enableProdMode} from 'angular2/core';
+import {enableProdMode} from 'angular2/core';
 import {HttpApp} from './submit_form';
 
-// enableProdMode();
+enableProdMode();
 bootstrap(HttpApp);
